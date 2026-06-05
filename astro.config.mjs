f772095d55f731b0725b6e6758dccf85870d470b
daito daison <watanabe328@outlook.com>
2026-06-05 14:49:42 +0900

    fix: /ja/blog 404修正・ja.properties追加・リンクバグ修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -75,7 +75,7 @@ export default defineConfig({
   ],
   i18n: {
     defaultLocale: DEFAULT_LOCALE,
-    locales: ["en", "ar", "fr", "de"],
+    locales: ["en", "ar", "fr", "de", "ja"],
     routing: { prefixDefaultLocale: true }
   },
   integrations: [
